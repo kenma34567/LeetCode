9382a8a8719ed274a81604ba83eb1d3892c4edd7
Ken Ma <kenmamc@gmail.com>
2023-10-17 19:38:39 +0900

    rewrite 88. Merge Sorted Array with comment

--- 88. Merge Sorted Array .py
+++ 88. Merge Sorted Array .py
@@ -4,19 +4,25 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
 
-        pointerNums1, pointerNums2, pointerWrite = m - 1, n - 1, m + n - 1
-        while pointerNums2 >= 0:
-            print("comparing", nums1[pointerNums1], nums2[pointerNums2])
+        """
+            Write from the end, use 2 pointers to compare element and write the larger one
+            If one of the array has run out of elements, write the remaining of the other
+        """
 
-            if pointerNums1 >= 0 and nums1[pointerNums1] > nums2[pointerNums2]:
-                print("writing pointer 1")
-                nums1[pointerWrite] = nums1[pointerNums1]
-                pointerNums1 -= 1
+        m, n = len(nums1) - len(nums2), len(nums2)
+        pNums1, pNums2, pWrite = m - 1, n - 1, m + n - 1
+
+        while pWrite > -1:
+
+            if pNums1 < 0 or (pNums2 > -1 and nums2[pNums2] >= nums1[pNums1]):
+                # print("writing 2", nums2[pNums2], pWrite)
+                nums1[pWrite] = nums2[pNums2]
+                pNums2 -= 1
             else:
-                print("writing pointer 2")
-                nums1[pointerWrite] = nums2[pointerNums2]
-                pointerNums2 -= 1
-            pointerWrite -= 1
+                # print("writing 1", pNums1)
+                nums1[pWrite] = nums1[pNums1]
+                pNums1 -= 1
+
+            pWrite -= 1
 
         print("ans", nums1)
-
