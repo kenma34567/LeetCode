483f98d6c423c1b1d710980fcfe7f06a4e3ca714
Ken Ma <kenmamc@gmail.com>
2024-01-08 15:46:37 +0900

    (Submission on Dec 13, 2023) revised 33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,93 +1,29 @@
-"""
-Runtime: Beats 98.4%
-Memory: Beats 21.1%
-Modified Binary Search For "flipped" nums
-"""
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            print("mid index", mid)
 
-        def doRotated(nums: List[int], target: int, left: int, right: int) -> int:
+            if nums[mid] == target:
+                return mid
 
-            while left < right:
-
-                mid = left + ((right - left) // 2)
-
-                print("checking", left, mid, right)
-
-                if nums[left] == target:
-                    return left
-                elif nums[right] == target:
-                    return right
-                elif nums[mid] == target:
-                    return mid
-
-                if left == mid or right == mid:
-                    break
-
-                """
-                    Use the left-half when
-                        1) target is within range of left & mid [4,5,6,1,2] find 5
-                        2) left > mid and target > left         [7,8,1,2,3] find 8
-                        3) mid > left and left > target         [5,1,2,3,4] find 1
-                """
-                if nums[mid] > target > nums[left] or target > nums[left] > nums[mid] or nums[left] > nums[
-                    mid] > target:
-                    right = mid
+            if nums[left] <= nums[mid]:
+                if nums[mid] < target or target < nums[left]:
+                    left = mid+1
                 else:
-                    left = mid
-
-            return -1
-
-        def doBinarySearch(nums: List[int], target: int, left: int, right: int) -> int:
-
-            while left < right:
-
-                mid = left + ((right - left) // 2)
-
-                if nums[left] == target:
-                    return left
-                elif nums[right] == target:
-                    return right
-                elif nums[mid] == target:
-                    return mid
-
-                if left == mid or right == mid:
-                    break
-
-                if nums[mid] > target:
-                    right = mid
-                else:
-                    left = mid
-
-            return -1
-
-        if len(nums) == 1:
-            return 0 if nums[0] == target else -1
-        elif len(nums) == 2:
-            if nums[0] == target:
-                return 0
-            elif nums[1] == target:
-                return 1
+                    right = mid-1
             else:
-                return -1
+                if target < nums[mid] or nums[right] < target:
+                    right = mid-1
+                else:
+                    left = mid+1
 
-        left, right = 0, len(nums) - 1
-        mid = left + ((right - left) // 2)
 
-        if nums[left] == target:
-            return left
-        elif nums[right] == target:
-            return right
-        elif nums[mid] == target:
-            return mid
+        return -1
 
-        rotated = nums[right] < nums[left]
-        print("nn", rotated)
 
-        if rotated:
-            print("do rotated", left, right)
-            return doRotated(nums, target, left, right)
-        else:
-            print("do normal")
-            return doBinarySearch(nums, target, left, right)
+        '''
+            [4,5,6,7,8,0,1,2], target = 8
+            [1,2,3,4,5,6], target = 4
+        '''
