93aa99b198880219dfa6dd058989d2ab120bc5c2
Ken Ma <kenmamc@gmail.com>
2024-01-08 15:51:38 +0900

    (Submission on Dec 15, 2023) 39. Combination Sum

---
+++ 39. Combination Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def backtrack(first: int, combination: []):
+
+            result = sum(combination)
+            if result == target:
+                ans.append(combination[:])
+                return
+
+            for i in range(first, len(candidates)):
+                c = candidates[i]
+                if result + c > target:
+                    continue
+
+                combination.append(c)
+                backtrack(i, combination)
+                combination.pop()
+                backtrack(i + 1, combination)
+
+        ans = []
+        backtrack(0, [])
+
+        return ans
