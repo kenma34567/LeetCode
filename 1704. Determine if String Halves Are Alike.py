463e60c0fd3a85939f0c3b0eec12bcffee637465
Ken Ma <kenmamc@gmail.com>
2024-01-14 13:55:40 +0900

    (Submission on Jan 12, 2024) rewrite 1704. Determine if String Halves Are Alike

--- 1704. Determine if String Halves Are Alike.py
+++ 1704. Determine if String Halves Are Alike.py
@@ -1,24 +1,16 @@
-"""
-Runtime: 49 ms Beats 76.88%
-Memory: 13.9 MB Beats 77.72%
-"""
-
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        vowelsSet = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
-        mid = len(s) // 2
-        firstHalf = s[:mid]
-        secondHalf = s[mid:]
+
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        s = s.lower()
 
         count = 0
-        for s in firstHalf:
-            if s in vowelsSet:
+        for i in range(len(s)//2):
+            if s[i] in vowels:
                 count += 1
 
-        for s in secondHalf:
-            if s in vowelsSet:
+        for i in range(len(s)//2, len(s)):
+            if s[i] in vowels:
                 count -= 1
 
         return count == 0
-
-
