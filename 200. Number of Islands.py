d698b9de1a01ed1a0e86a8749ac6b3153a5591d9
Ken Ma <kenmamc@gmail.com>
2024-01-14 13:51:37 +0900

    (Submission on Jan 8, 2024) rewrite 200. Number of Islands

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -1,33 +1,35 @@
-"""
-Runtime: 534 ms Beats 37.89%
-Memory: 16.4 MB Beats 65.31%
-"""
-
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        ''' if "1" is found, turn all the "1"s that is connected to it recursively'''
+        ans = 0
+        visited = set()
 
-        def islandToWater(i, j):
+        def bfs(i, j):
 
-            ''' Turn Left, Right, Top, Bottom to "Water" recursively until all adjacent element are "0"s '''
-            if j < 0 or j == len(grid[0]) or i < 0 or i == len(grid) or grid[i][j] == "0":
-                return
-            grid[i][j] = "0"
+            q = collections.deque()
+            q.append((i, j))
 
-            islandToWater(i, j - 1)  # Left
-            islandToWater(i, j + 1)  # Right
-            islandToWater(i - 1, j)  # Top
-            islandToWater(i + 1, j)  # Bottom
+            # top, bottom, left, right
+            moveDir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        count = 0
+            while q:
+                current = q.popleft()
+                visited.add(current)
+                for move in moveDir:
+                    moveI, moveJ = current[0] + move[0], current[1] + move[1]
+                    if ((moveI, moveJ) not in visited and
+                            moveI in range(len(grid)) and
+                            moveJ in range(len(grid[moveI])) and
+                            grid[moveI][moveJ] == "1"):
+                        q.append((moveI, moveJ))
+                        visited.add((moveI, moveJ))
+
+
         for i in range(len(grid)):
-            for j in range(len(grid[0])):  # same for each line
+            for j in range(len(grid[i])):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    bfs(i, j)
+                    ans += 1
+                visited.add((i, j))
 
-                current = grid[i][j]
-                if current == "1":
-                    count += 1
-                    islandToWater(i, j)
-
-        return count
+        return ans
