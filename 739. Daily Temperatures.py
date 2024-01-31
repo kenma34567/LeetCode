13ca25a898951398979af065409ea11c7317e29b
Ken Ma <kenmamc@gmail.com>
2024-01-31 15:44:49 +0900

    Revised 739. Daily Temperatures

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -2,16 +2,9 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack, ans = [], [0] * len(temperatures)
         for i, t in enumerate(temperatures):
-            if not stack:
-                stack.append((i, t))
-                continue
-            index, temp = stack[-1][0], stack[-1][1]
-            while t > temp:
-                stack.pop()
+            while stack and t > stack[-1][1]:
+                index, temp = stack.pop()
                 ans[index] = i - index
-                if not stack:
-                    break
-                index, temp = stack[-1][0], stack[-1][1]
             stack.append((i, t))
 
         # print("ans", ans)
